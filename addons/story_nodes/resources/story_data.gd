d72fb7928608468991baa403521cbf481541bda2
Jim Hessin <jhessin@gmail.com>
2026-09-12 17:15:50 -0600

    Step 9: Add simple link api

--- addons/story_nodes/resources/story_data.gd
+++ addons/story_nodes/resources/story_data.gd
@@ -4,6 +4,14 @@ extends Resource
 @export var title: String = ''
 @export var description: String = ''
 
-@export var nodes: Array[StoryNode] = []
+var nodes: Array[StoryNode] = []
 
 var links: Dictionary[StoryLink, Object] = { }
+
+
+func add_link(from: StringName, to: StringName) -> StoryLink:
+	var link := StoryLink.new(from, to)
+
+	links[link] = null
+
+	return link
--- addons/story_nodes/resources/story_link.gd
+++ addons/story_nodes/resources/story_link.gd
@@ -3,3 +3,8 @@ extends RefCounted
 
 var from: StringName = &""
 var to: StringName = &""
+
+
+func _init(_from: StringName = &'', _to: StringName = &'') -> void:
+	from = _from
+	to = _to

cb7840d4d7033a7f488bfeace9ff0f0b8a4060a4
Jim Hessin <jhessin@gmail.com>
2026-09-12 17:26:26 -0600

    Step 14: Expanded StoryData API

--- addons/story_nodes/resources/story_data.gd
+++ addons/story_nodes/resources/story_data.gd
@@ -15,3 +15,43 @@ func add_link(from: StringName, to: StringName) -> StoryLink:
 	links[link] = null
 
 	return link
+
+
+func remove_link(link: StoryLink) -> void:
+	links.erase(link)
+
+
+func get_link(from: StringName, to: StringName) -> StoryLink:
+	for link: StoryLink in links:
+		if link.from == from and link.to == to:
+			return link
+
+	return null
+
+
+func get_links_from(from: StringName) -> Array[StoryLink]:
+	var results: Array[StoryLink] = []
+
+	for link: StoryLink in links:
+		if link.from == from:
+			results.append(link)
+
+	return results
+
+
+func get_links_to(to: StringName) -> Array[StoryLink]:
+	var results: Array[StoryLink] = []
+
+	for link: StoryLink in links:
+		if link.to == to:
+			results.append(link)
+
+	return results
+
+
+func get_node(id: StringName) -> StoryNode:
+	for node: StoryNode in nodes:
+		if node.id == id:
+			return node
+
+	return null

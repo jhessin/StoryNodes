c4435fdf392f46cb50ef1391d1d5a9d11a2f540f
Jim Hessin <jhessin@gmail.com>
2026-09-23 12:03:38 -0600

    update ConditionRow

--- addons/story_nodes/nodes/condition_row.gd
+++ addons/story_nodes/nodes/condition_row.gd
@@ -32,6 +32,21 @@ func set_variable(value: StoryVariable) -> void:
 
 	_populate_operator_picker()
 
+	match variable.type:
+		StoryVariable.Type.STRING:
+			condition.operator = BranchCondition.Operator.IS_EMPTY
+			condition.value = ''
+
+		StoryVariable.Type.INT, StoryVariable.Type.FLOAT:
+			condition.operator = BranchCondition.Operator.EQUAL
+			condition.value = 0
+
+		StoryVariable.Type.BOOL:
+			condition.operator = BranchCondition.Operator.EQUAL
+			condition.value = false
+
+	operator_picker.select(condition.operator)
+
 
 func _populate_operator_picker() -> void:
 	operator_picker.clear()

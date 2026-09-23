2f27f73398f0ee80e6a4a2aa7c9c6580914caa8c
Jim Hessin <jhessin@gmail.com>
2026-09-23 11:36:02 -0600

    add condition_row scene.

--- addons/story_nodes/nodes/branch_graph_node.gd
+++ addons/story_nodes/nodes/branch_graph_node.gd
@@ -6,6 +6,12 @@ var branch_node: BranchNode:
 	get:
 		return story_node as BranchNode
 
+@onready var variable_picker: OptionButton = %VariablePicker
+
+
+func _ready() -> void:
+	variable_picker.item_selected.connect(_on_variable_selected)
+
 
 func set_story_node(node: StoryNode) -> void:
 	if not node is BranchNode:
@@ -13,3 +19,7 @@ func set_story_node(node: StoryNode) -> void:
 		return
 
 	super.set_story_node(node)
+
+
+func _on_variable_selected() -> void:
+	pass

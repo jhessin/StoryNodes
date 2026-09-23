65430690fcfe423ca10b293cbdad2726836d361a
Jim Hessin <jhessin@gmail.com>
2026-09-23 11:46:26 -0600

    add condition to ConditionRow

--- addons/story_nodes/nodes/condition_row.gd
+++ addons/story_nodes/nodes/condition_row.gd
@@ -2,5 +2,17 @@
 class_name ConditionRow
 extends HBoxContainer
 
+var condition: BranchCondition
+
 @onready var operator_picker: OptionButton = %OperatorPicker
 @onready var value_field: LineEdit = %ValueField
+
+
+func set_condition(value: BranchCondition) -> void:
+	condition = value
+
+	if condition == null:
+		push_error('ConditionRow requires a BranchCondition')
+		return
+
+	operator_picker.select(condition.operator)

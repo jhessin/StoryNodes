a74cb1d05608cd855081d6b819c82c9db9f6c019
Jim Hessin <jhessin@gmail.com>
2026-09-22 19:07:26 -0600

    update move_choice() in ChoiceNode

--- addons/story_nodes/resources/nodes/choice_node.gd
+++ addons/story_nodes/resources/nodes/choice_node.gd
@@ -3,3 +3,20 @@ class_name ChoiceNode
 extends StoryNode
 
 @export var choices: Array[String] = []
+
+
+func move_choice(from_index: int, to_index: int) -> void:
+	if from_index < 0 or from_index >= choices.size():
+		return
+
+	if to_index < 0 or to_index >= choices.size():
+		return
+
+	if from_index == to_index:
+		return
+
+	var choice: String = choices[from_index]
+	choices.remove_at(from_index)
+	choices.insert(to_index, choice)
+
+	emit_changed()

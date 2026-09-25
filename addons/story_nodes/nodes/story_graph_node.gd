d19bbf1c68ced592bf4f0556e15dc2e7b7362eca
Jim Hessin <jhessin@gmail.com>
2026-09-25 11:01:48 -0600

    update StoryGraphNode

--- addons/story_nodes/nodes/story_graph_node.gd
+++ addons/story_nodes/nodes/story_graph_node.gd
@@ -28,6 +28,9 @@ func set_story_node(node: StoryNode) -> void:
 	title = story_node.display_name + '(' + story_node.instance_id + ')'
 	position_offset = story_node.position
 
+	if story_node.size != Vector2.ZERO:
+		size = story_node.size
+
 
 func set_story_data(data: StoryData) -> void:
 	story_data = data
